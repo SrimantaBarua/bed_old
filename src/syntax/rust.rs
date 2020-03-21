820800a8f06af7ca1b06660e507dd714ea87d6ad
Srimanta Barua <srimanta.barua1@gmail.com>
2020-03-21 13:56:56 +0530

    Highlight true/false in Rust code

--- src/syntax/rust.rs
+++ src/syntax/rust.rs
@@ -415,6 +415,7 @@ fn key_or_ident(s: &str) -> RustTok {
         "mut" => RustTok::KeyMut,
         "bool" | "char" | "i8" | "i16" | "i32" | "i64" | "i128" | "isize" | "u8" | "u16"
         | "u32" | "u64" | "u128" | "usize" | "f32" | "f64" | "str" => RustTok::KeyTyp,
+        "true" | "false" => RustTok::Num,
         _ => RustTok::Ident,
     }
 }
